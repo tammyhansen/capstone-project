2a4b1f9786eecaf91f21720ce7120bbddbf80b72
SSA-Vale <114950732+SSA-Vale@users.noreply.github.com>
2023-04-25 20:33:54 -0700

    Canvas.js update
    added fetch for world data and started implementing drawing of objects with draw() function.

--- static/canvas.js
+++ static/canvas.js
@@ -7,6 +7,7 @@ function drawObject(ctx, x, y, size) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, 2*Math.PI);
 }
+//basic grid layout for play area
 class Grid {
     constructor(x, y, height, width) {
         this.map = [];
@@ -33,8 +34,52 @@ class Grid {
         };
     }
 }
+//drawing ships on screen
+class Ship {
+    constructor(ship, x, y, style) {
+        this.ship = ship;
+        this.x = x;
+        this.y = y;
+        this.style = style;
+        this.draw = function(c, sytle){
+
+        }
+    }
+}
+function draw(data) {
+    let c = getCanvas();
+    for (var objId of Object.values(data.world.objects)) {
+        if(objId == "ship"){
+            c.fillStyle = "blue";
+            c.beginPath();
+            c.moveTo(objId.position.x + 10, objId.position.y);
+            c.lineTo(objId.position.x + 20, objId.position.y + 20);
+            c.lineTo(objId.position.x + 10, objId.position.y + 15);
+            c.lineTo(objId.position.x, objId.position.y + 20);
+            c.closePath();
+            c.fill();
+            c.stroke();
+        }
+        else{
+            c.fillStyle = "red";
+            c.beginPath();
+            c.moveTo(objId.position.x + 10, objId.position.y);
+            c.lineTo(objId.position.x + 20, objId.position.y + 20);
+            c.lineTo(objId.position.x + 10, objId.position.y + 15);
+            c.lineTo(objId.position.x, objId.position.y + 20);
+            c.closePath();
+            c.fill();
+            c.stroke();
+        }
+    }
+}
 const ctx = getCanvas();
 grid = new Grid(20, 20, 20, 20);
 grid.draw(ctx);
 drawObject(ctx, 30, 30, 10);
 ctx.stroke();
+fetch('/api/game').then(response => response.json()).then(json => {
+    let data = json;
+    console.log(data);
+    draw(data);
+})
