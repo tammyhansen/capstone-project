8854c01ac63687079c3879914367e63b70c8539a
Tammy Hansen <94862936+tammyhansen@users.noreply.github.com>
2023-05-01 18:55:27 -0700

    corrected objType to Enum

--- model/Object.py
+++ model/Object.py
@@ -2,7 +2,7 @@ from model.Components.Position  import Position
 from model.Components.Storage   import Storage
 from enum import Enum
 
-class ObjectType(Enum):
+class objType(Enum):
     Ship        = 'Ship',
     Asteroid    = 'Asteroid',
     Comet       = 'Comet',
@@ -14,14 +14,14 @@ class ObjectType(Enum):
 
 class Object:
     objId       : str
-    objType     : str
+    objType     : Enum
 
     position    : Position
     storage     : Storage
 
     owner       : "str|None"
 
-    def __init__(self, objType: str) -> None:
+    def __init__(self, objType: Enum) -> None:
         self.objType  = objType
         self.position = Position(0, 0, None)
         self.storage  = Storage(0)
