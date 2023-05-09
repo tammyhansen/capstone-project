db65a0f8c6f06889d85943d9dc13761772f0bf02
WilliamDann <wcdann2@gmail.com>
2023-05-08 20:51:38 -0700

    linked engine to game thread

--- engine/main.py
+++ engine/main.py
@@ -1,8 +1,18 @@
-from pymongo.database   import Database
-from Game               import Game
+from pymongo.database       import Database
+from Game                   import Game
+from engine.runner.main     import runner
+from logging                import info
 
 # Run a tick of the game
 def tick(db: Database, game: Game):
+    info(f"Started tick {game.time}")
+
+    # Run user scripts
     users = db['users'].find({})
     for user in users:
-        pass
+        runner(user)
+
+    info(f"Finished tick {game.time}")
+
+    # Update game tick
+    game.time += 1
--- engine/runner/main.py
+++ engine/runner/main.py
@@ -1,12 +1,15 @@
+from model.User             import User
+from model.World            import World
+
 from engine.runtime.runtime import runtime
 from logging                import info, debug
 
-def runner(user):
+def runner(user: User, world: World):
     if not user.userCode:
         return
 
     info(f"Started script '{user.username}'")
-    userRuntime = runtime()
+    userRuntime = runtime(user.username, world)
     try:
         exec(user.userCode,
             {"__builtins__": None},
