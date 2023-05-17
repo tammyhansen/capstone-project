5c6a3206588ed19612b979fe429c6ea9f771d8a9
lwtech-diannes <diannegermaine@gmail.com>
2023-05-16 20:49:37 -0700

    added bounds checking

--- engine/runtime/intents/MoveIntent.py
+++ engine/runtime/intents/MoveIntent.py
@@ -9,27 +9,14 @@ class MoveIntent:
     newPosition : Position
 
     def run(self, world: World):
-        # TODO bounds checking
-
-        # Boundaries
-        min_x = 1
-        max_x = 1
-        min_y = 1
-        max_y = 1
-
-        cur_x = 0
-        cur_y = 0
-
-        for obj in world.objects:
-            if obj == self.objId:
-                cur_x = obj.position.x
 
         # Perform bounds checking
-        if self.newPosition.x < self.objId.positon.x + max_x or self.newPosition.x > self.objId.position.x - min_x:
+        obj = world.objects[self.objId]
+        if abs(obj.position.x - self.newPosition.x) > 1:
             info(f'Error: X-coordinate out of bounds for object {self.objId}')
             return
 
-        if self.newPosition.y < min_y or self.newPosition.y > max_y:
+        if abs(obj.position.y - self.newPosition.y) > 1:
             info(f'Error: Y-coordinate out of bounds for object {self.objId}')
             return
     
