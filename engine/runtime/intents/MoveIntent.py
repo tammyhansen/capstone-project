254812e66e28b9626c73eb12a563bd66ae13b29a
lwtech-diannes <diannegermaine@gmail.com>
2023-05-16 20:02:34 -0700

    committing temporarily...

--- engine/runtime/intents/MoveIntent.py
+++ engine/runtime/intents/MoveIntent.py
@@ -10,6 +10,28 @@ class MoveIntent:
     def run(self, world):
         # TODO bounds checking
 
+        # Boundaries
+        min_x = 1
+        max_x = 1
+        min_y = 1
+        max_y = 1
+
+        cur_x = 0
+        cur_y = 0
+
+        for obj in world.objects:
+            if obj == self.objId:
+                cur_x = obj.position.x
+
+        # Perform bounds checking
+        if self.newPosition.x < self.objId.positon.x + max_x or self.newPosition.x > self.objId.position.x - min_x:
+            info(f'Error: X-coordinate out of bounds for object {self.objId}')
+            return
+
+        if self.newPosition.y < min_y or self.newPosition.y > max_y:
+            info(f'Error: Y-coordinate out of bounds for object {self.objId}')
+            return
+    
         # Update the position
         info(f'Move intent executed {self.objId} to {self.newPosition}')
         world.objects[self.objId].position = self.newPosition
